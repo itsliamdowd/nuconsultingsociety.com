4d2c5a458c4c351253764a2f7b6e76b8dd18ab26
Liam Dowd <101684827+itsliamdowd@users.noreply.github.com>
2025-02-11 15:51:22 +0000

    Add files via upload

---
+++ framerusercontent.com/sites/6EnH7GO1cmp0sc5JD17mgG/chunk-GIBL5YA4.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"Northeastern University London & Oakland Consulting Society",favicon:"https://framerusercontent.com/assets/lE43ssdYJJy5ORUrQS9rSCBBnEg.png",robots:"max-image-preview:large",title:"NU Consulting Society"}}export{t as a};
+//# sourceMappingURL=chunk-GIBL5YA4.mjs.map
